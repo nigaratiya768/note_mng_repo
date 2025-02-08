3c780de2c30e19ef8953b837a8a37d09fad57f01
atiya nigar <nigaratiya786@gmail.com>
2025-02-08 22:24:06 +0530

    adding curd operatin

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { addNotes } = require("./controller/notes_control");
+const {
+  addNotes,
+  getNotes,
+  getNote,
+  updateNote,
+  deleteNote,
+} = require("./controller/notes_control");
 const { connectDB } = require("./connect_database/connect_withDB");
 const app = express();
 connectDB();
@@ -8,6 +14,11 @@ app.use(express.json());
 const PORT = 4000;
 
 app.post("/api/notes", addNotes);
+app.get("/api/notes", getNotes);
+app.get("/api/notes/:id", getNote);
+app.put("/api/note/:id", updateNote);
+app.delete("/api/notes/id", deleteNote);
+
 app.listen(PORT, () => {
   console.log("server is running");
 });
